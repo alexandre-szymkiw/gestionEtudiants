592d41582f0694b0f82fe8be979c036e0d9f5abd
simcmoi <simonfessy@gmail.com>
2017-03-15 14:11:05 +0100

    Create absents.h

---
+++ absents.h
@@ -0,0 +1,1 @@
+#ifndef ABSENCES_H_INCLUDED#define ABSENCES_H_INCLUDEDtypedef struct Absences Absences;struct Absences{    double **abs;    char *a;    Absences *suivant;};typedef struct PileAbsences PileAbsences;struct PileAbsences{    Absences *premier;};PileAbsences *initialiserAbsences();#endif // ABSENCES_H_INCLUDED
